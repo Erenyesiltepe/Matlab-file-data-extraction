4fb01510af8fa9e6d59a0ecf509c8111845c4d03
Eren Yeşiltepe <eren.bilkentk15@hotmail.com>
2024-10-11 15:37:31 +0300

    datas removed

--- test.py
+++ test.py
@@ -35,8 +35,8 @@ def load_recall_data(data_path):
         'Response': responses,
         'Time': time,
         'Rating': rating,
-        'SeenPoint': ['-' for _ in data],
-        'NotSeenPoint': ['-' for _ in data],
+        'SeenPoint': [None for _ in data],
+        'NotSeenPoint': [None for _ in data],
     })
     return df    
 
@@ -56,19 +56,40 @@ def runForSubject(directory):
     
     exp_recall_data = load_recall_data(os.path.join(directory, recall_data_file))
     exp_data = load_data(os.path.join(directory, data_file))
+    
+    sum_seen = 0   
+    sum_not_seen = 0
 
     for index, row in exp_recall_data.iterrows():
         matching_rows = exp_data[exp_data['BiomotionName'] == row['BiomotionName']]
         if matching_rows.empty:
             if row['Response'] == 'x':
-                exp_recall_data.at[index, 'NotSeenPoint'] = '0'
+                exp_recall_data.at[index, 'NotSeenPoint'] = 0
             else:
-                exp_recall_data.at[index, 'NotSeenPoint'] = '1'
+                exp_recall_data.at[index, 'NotSeenPoint'] = 1
+                sum_not_seen += 1
         else:
             if row['Response'] == 'x':
-                exp_recall_data.at[index, 'SeenPoint'] = '1'
+                exp_recall_data.at[index, 'SeenPoint'] = 1
+                sum_seen += 1
             else:
-                exp_recall_data.at[index, 'SeenPoint'] = '0'
+                exp_recall_data.at[index, 'SeenPoint'] = 0
+                
+        
+    # Add two separate rows at the end
+    new_row_1 = {
+        'Category': 'Total',
+        'SeenPoint': sum_seen,
+        'NotSeenPoint': sum_not_seen
+    }
+    new_row_2 = {
+        'Category': 'Percentage',
+        'SeenPoint': sum_seen / 35,
+        'NotSeenPoint': sum_not_seen / 14
+    }
+    exp_recall_data = pd.concat([exp_recall_data, pd.DataFrame([new_row_1])], ignore_index=True)
+    exp_recall_data = pd.concat([exp_recall_data, pd.DataFrame([new_row_2])], ignore_index=True)
+
 
     exp_recall_data.to_excel(os.path.join(directory, f'ResultExperimentData-{subject}.xlsx'), index=False)
 
